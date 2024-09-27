e2a49c7cfacbcb27608d01005d6184735c2d2b05
AURELAZICO <aurelazico981@gmail.com>
2024-09-27 10:47:15 +0700

    Add files via upload

---
+++ BIODATA DIRI.py
@@ -0,0 +1,11 @@
+nama_depan = "Aurel"
+nama_belakang = 'Azico'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = '18'
+alamat = "Toboali"
+kata_mutiara = " Be yourself and never surrender"
+print(nama_lengkap, '(' + usia + ')' , ',' , 'dari' , alamat, ', kata mutiara:' , kata_mutiara)
+print("\nTipe dari nama_lengkap:" , type (nama_lengkap))
+print(" Tipe dari usia:" , type(usia))
+print("Tipe dari alamat:" , type (alamat))
+print("Tipe dari kata_mutiara:" , type(kata_mutiara))
